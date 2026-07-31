c2c9492a6d68a4ea9bf05ca4e5d89213f448d24d
yoginibhatia2-debug <yoginibhatia2@gmail.com>
2026-07-31 11:58:20 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+player.onChat("run", function () {
+	
+})
+function volumeofcuboid (l: number, b: number, h: number) {
+    volume = l * (b * h)
+    player.say("volume of cuboid is ")
+    player.say(volume)
+}
+let volume = 0
+volume = 0
+volumeofcuboid(5, 6, 10)
